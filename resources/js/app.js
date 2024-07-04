c3c0dafac4103e83b5d3199f032e2bd6a9d38cf5
Timmy <timisutantyo@gmail.com>
2024-07-04 21:42:13 +0200

    Add comments about advanced JS concepts

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import MainLayout from '@/Layouts/MainLayout.vue'
 
 createInertiaApp({
+  // async: promises. Those functions work in the background. When ready return some function
   resolve: async (name) => {
+    // we can import multiple module
     const pages = import.meta.glob('./Pages/**/*.vue')
     const page = await pages[`./Pages/${name}.vue`]()
     page.default.layout = page.default.layout  || MainLayout
@@ -11,6 +13,10 @@ createInertiaApp({
     return page
 
   },
+  // normal function:
+  // function () {}
+  // this keyword: depends on how the function was called. Arrow function refers to the parent
+  // We pass an object to the setup method
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props)})
       .use(plugin)
